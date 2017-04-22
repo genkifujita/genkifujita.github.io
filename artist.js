eb6a52156230f41c1e67d4d38dabb137128250cd
genkifujita <fujitainkree@gmail.com>
2017-04-23 00:02:52 +0900

    HOME画像変更

--- artist.js
+++ artist.js
@@ -1,8 +1,35 @@
 $(function() {
- $("#login-show").click(function(){
-   $("#login-modal").fadeIn();
+  $('.genki-fujita-1').click(function(){
+    $('#login-modal').fadeIn();
+  });
 
 
- });
+  $(".close-modal").click(function(){
+    $('#login-modal').fadeOut();
+  });
+
+});
+
+$(function() {
+  $('.genki-fujita-2').click(function(){
+    $('#login-modal-2').fadeIn();
+  });
+
+
+  $(".close-modal").click(function(){
+    $('#login-modal-2').fadeOut();
+  });
+
+});
+
+$(function() {
+  $('.genki-fujita-3').click(function(){
+    $('#login-modal-3').fadeIn();
+  });
+
+
+  $(".close-modal").click(function(){
+    $('#login-modal-3').fadeOut();
+  });
 
 });
